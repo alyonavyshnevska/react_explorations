471902c89cd7f25fcbaa00407313dce8a64d69d6
Alyona Vyshnevska <alyona.vyshnevska@gmail.com>
2021-06-19 18:07:53 +0200

    fix typo and a tag

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 const About = () => {
     return (
         <div>
-<h4>A simple Task Tracker built with functional components, routing, and a json server. </h4>
+<p>A simple Task Tracker built with functional components, routing, and a json server. </p>
 {/* use link instead of a so that page does not reload: quicker */}
 <Link to='/'>Go Back to Tasks</Link>
         </div>
